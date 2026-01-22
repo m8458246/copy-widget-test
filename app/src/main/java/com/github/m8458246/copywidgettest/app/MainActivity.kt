0a9d3c8887920492b10ef827ac2942b35afbfd58
ViSt <61178698+m8458246@users.noreply.github.com>
2026-01-22 18:58:04 +0800

    更新 MainActivity.kt

--- app/src/main/java/com/github/m8458246/copywidgettest/app/MainActivity.kt
+++ app/src/main/java/com/github/m8458246/copywidgettest/app/MainActivity.kt
@@ -1,43 +1,12 @@
-package com.github.m8458246.copywidgettest.app
+package com.example.simplecopy
 
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import com.github.m8458246.copywidgettest.app.databinding.ActivityMainBinding
-import com.github.m8458246.copywidgettest.library.FactorialCalculator
-import com.github.m8458246.copywidgettest.library.android.ToastUtil
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.buttonCompute.setOnClickListener {
-            val message = if (binding.editTextFactorial.text.isNotEmpty()) {
-                val input = binding.editTextFactorial.text.toString().toLong()
-                val result = try {
-                    FactorialCalculator.computeFactorial(input).toString()
-                } catch (ex: IllegalStateException) {
-                    "Error: ${ex.message}"
-                }
-
-                binding.textResult.text = result
-                binding.textResult.visibility = View.VISIBLE
-                getString(R.string.notification_title, result)
-            } else {
-                getString(R.string.please_enter_a_number)
-            }
-            ToastUtil.showToast(this, message)
-        }
-
-        binding.buttonAppcompose.setOnClickListener {
-            val intent = Intent(it.context, ComposeActivity::class.java)
-            startActivity(intent)
-        }
+        // 这里什么都不用做，我们只需要 Widget 功能
+        // 甚至连 setContentView 都不需要，打开就是一个白屏然后可以关掉
     }
 }
